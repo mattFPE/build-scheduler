a0f8bb431ea84716d7e82df1ba25ddfa1da42af8
Matt Carter <matt.carter@fleeceperformance.com>
2023-11-13 10:40:13 -0500

    Update

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <!-- <header>
-    <h1>Site Wide Header</h1>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header> -->
   <div class="flex flex-column mt-8">
     <h1>Build Scheduler</h1>
     <RouterView />
